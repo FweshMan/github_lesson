0dfe9567398611d61ab9e40f10aef9ce19311d54
Owen Ho <owenho0314@gmail.com>
2024-04-17 22:04:38 +0000

    I put my Beatles Fanpage

--- sketch.js
+++ sketch.js
@@ -1,40 +1,32 @@
-function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(220);
-    background("green")
-    strokeWeight(5)
-    fill('white')
-    ellipse(175, 300, 50, 30)
-    ellipse(225, 300, 50, 30)
-    fill('yellow')
-    ellipse(165, 250, 30, 50)
-    ellipse(235, 250, 30, 50)
-    rect(160, 210, 80, 80)
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <style>
+@import url('https://fonts.googleapis.com/css2?family=Bahianita&family=Barriecito&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap')
+</style>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.0/p5.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.0/addons/p5.sound.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8" />
+   <style>
+  @import url('https://fonts.googleapis.com/css2?family=Honk&display=swap');
+</style>
+
+  </head>
+  <body>
+    <main>
+      <h1 class="title">The Beatles</h1>
+      <p>The 1960 band "The Beatles" are arguably one of most influential and iconic band in the history of pop music. Their unique sound,  songwriting, and culture continues to impact fans and new generations around the world.</p>
+      <h2 class="subtitle">About The Beatles</h2>
+      <div class="outer_box">
+        <img class="img_one" src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/09/17/17/the-beatles.jpg"/>
+      </div>
+       <p>The Beatles were a band formed in Liverpool in 1960 comprised of John Lennon, the founder, co-lead vocalist and guitarist, Paul McCartney, the bassist and co-lead vocalist, George Harrison, the other guitarist and co-lead vocalist,and Ringo Starr, the drummer. They all played a key role in songwriting and vocals. This group of 4 would go on to change the world of music with their revolutionary and experimental music.</p>
+      <h2 class="subtitle2">Greatest Hits</h2>
+      <p>The Beatles are home to many hits and recognizable songs. Their most popular ones are the following: <ol>Here Comes The Sun - Abbey Road (1969)</ol> <img class="img_one" src="https://cdn.mos.cms.futurecdn.net/XoffSAganT2JCWuZVT6gLd.jpg"/><ol>Come Together - Abbey Road (1969)</ol><img class="img_one" src="https://cdn.mos.cms.futurecdn.net/XoffSAganT2JCWuZVT6gLd.jpg"/><ol>Let It Be (1970)</ol><img class="img_one" src="https://upload.wikimedia.org/wikipedia/en/2/25/LetItBe.jpg"/><ol>Hey Jude (1968)</ol><img class="img_one" src="https://m.media-amazon.com/images/M/MV5BZDRiNjliMzYtMzJjNi00ZmQ3LWIyYTAtMGVkMDE4ODVjYjI4XkEyXkFqcGdeQXVyMjUyNDk2ODc@._V1_.jpg"/><ol>Twist and Shout (1963)</ol><img class="img_one" src="https://upload.wikimedia.org/wikipedia/en/e/eb/TwistandShoutBeatlesLPalbumcover.gif"/><ol>Their most popular albums are "Sgt. Pepper's Lonely Hearts Club Band" and "Abbey Road"</ol></p>
+    <h2 class="subtitle2">Where Are They Now?</h2>
+    Founder of The Beatles John Lennon was tragically shot in an assasination on December 8th, 1980. Other band member George Harrison also died in 2001 due to numerous cancers, being a lifelong smoker. The remaining members are Paul McCarteny and Ringo Starr. While the band is discontinued and 2 of the band members have passed away, the legacy they left behind will have an impact on music for many years to come, and the impact they already made has forever changed the music genre.
+    </main>
     
-    
-    
-    triangle(165, 220, 200, 230, 185, 240)
-    triangle(235, 220, 200, 230, 215, 240)
-    fill('white')
-    strokeWeight(3)
-    ellipse(200, 242, 10, 10)
-    strokeWeight(5)
-    ellipse(170, 115, 45, 80)
-    ellipse(230, 115, 45, 80)
-    ellipse(200, 180, 120, 100)
-    strokeWeight(0)
-  
-    ellipse(175, 140, 35, 25)
-    ellipse(225, 140, 35, 25)
-    fill('black')
-    ellipse(180, 185, 10, 10)
-    ellipse(220, 185, 10, 10)
-    strokeWeight(5)
-    line(195, 200, 205, 205);
-    line(195, 205, 205, 200);
-    
-    
-  }
+  </body>
+</html>
